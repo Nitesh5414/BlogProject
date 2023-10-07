bf93a6d739ddab258ddcd5ee4030c34418cefb9a
nitesh.bangar@playerzpot.com <niteshbangar2012@gmail.com>
2023-10-07 21:56:20 +0530

    changes som settings

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,3 +154,5 @@ EMAIL_HOST_USER = 'niteshbangar2012@gmail.com' # email id
 EMAIL_HOST_PASSWORD = 'ssjloqgzuqriuwep' #password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+django_heroku.settings(locals())
